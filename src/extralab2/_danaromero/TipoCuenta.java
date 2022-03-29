18a448c9641bbd16d1c2e301439e1e90bb8f24c0
dromero000 <d.romero@unitec.edu>
2022-03-29 10:23:22 -0600

    Enum

--- src/extralab2/_danaromero/TipoCuenta.java
+++ src/extralab2/_danaromero/TipoCuenta.java
@@ -9,13 +9,13 @@ package extralab2._danaromero;
  *
  * @author Dana Romero
  */
-public class ExtraLab2_DanaRomero {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public enum TipoCuenta {
+    NROMAL(0.02,500), PLANILLA(0,200), VIP(0.04,1000);
+    public double tasaInteres, minSaldo;
+    
+    TipoCuenta (double tasa, double saldo){
+        tasaInteres = tasa; 
+        minSaldo = saldo;
     }
     
 }
